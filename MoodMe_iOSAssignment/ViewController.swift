5062a11566ac17527e76750376b07fa18a443784
Tatsuya Moriguchi <tatsuya.moriguchi@icloud.com>
2024-10-30 17:15:03 -0700

    Initial Commit

---
+++ MoodMe_iOSAssignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MoodMe_iOSAssignment
+//
+//  Created by Tatsuya Moriguchi on 10/30/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
